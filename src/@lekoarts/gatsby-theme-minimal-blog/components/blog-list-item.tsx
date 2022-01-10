979cee7f281ef0e318e3993bb94d2de93d6b92a8
youthfulhps <ybh942002@gmail.com>
2022-01-10 11:38:53 +0900

    Modified minor styles

--- src/@lekoarts/gatsby-theme-minimal-blog/components/blog-list-item.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/blog-list-item.tsx
@@ -54,7 +54,7 @@ const BlogListItem = ({ post, showTags = true }: BlogListItemProps) => (
     <StyledTLink
       as={Link}
       to={post.slug}
-      sx={{ fontSize: [1, 2, 3], color: `text` }}
+      sx={{ fontSize: [2, 2, 3], color: `text` }}
     >
       {post.title}
     </StyledTLink>
--- src/@lekoarts/gatsby-theme-minimal-blog/styles/code.ts
+++ src/@lekoarts/gatsby-theme-minimal-blog/styles/code.ts
@@ -25,6 +25,9 @@ const code = {
         pl: 3,
       },
     },
+    "@media screen and (max-width: 768px)": {
+      fontSize: "12px",
+    },
   },
   ".gatsby-highlight[data-language=''], .gatsby-highlight[data-language='noLineNumbers']":
     {
