397dae5491794d60df4040686ef08c168017b539
youthfulhps <ybh942002@gmail.com>
2022-05-15 18:54:05 +0900

    config: 메타 소개글 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: `Youthfulhps.dev`,
   description: `Blog posted front-end, troubleshooting!`,
   author: `youthfulhps`,
-  introduction: `프론트엔드 주니어 개발자, 트러블슈팅에 대한 고민의 흔적을 기록으로 남기는 것을 좋아하는 개발자`,
+  introduction: `프론트엔드 주니어 개발자, 트러블슈팅에 대한 고민의 흔적을 기록으로 남기는 것을 좋아합니다.`,
   siteUrl: `https://youthfulhps.github.io`, 
   social: {
     twitter: ``, 
