0d568796aab931a405c712e2b49a4a32f136ae45
youthfulhps <ybh942002@gmail.com>
2022-05-17 09:28:22 +0900

    update: siteUrl 메타 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `Blog posted front-end, troubleshooting!`,
   author: `youthfulhps`,
   introduction: `프론트엔드 주니어 개발자, 트러블슈팅에 대한 고민의 흔적을 기록으로 남기는 것을 좋아합니다.`,
-  siteUrl: `https://youthfulhps.github.io`, 
+  siteUrl: `https://youthfulhps.dev`, 
   social: {
     twitter: ``, 
     github: `youthfulhps`, 
