e5b2505404045efcbfe6c38a2416c379c434dd25
youthfulhps <ybh942002@gmail.com>
2023-03-20 13:22:13 +0900

    Chore: Remove console

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { TARGET_CLASS } from '../../utils/visible'
+import React from 'react';
+import { Link } from 'gatsby';
+import { TARGET_CLASS } from '../../utils/visible';
 
-import './index.scss'
+import './index.scss';
 
 export const ThumbnailItem = ({ node }) => {
-  console.log(node.frontmatter.description);
   return (
-
-  <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
-    <div key={node.fields.slug}>
-      <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <p dangerouslySetInnerHTML={{ __html: node.frontmatter.description || node.excerpt }} />
-    </div>
-  </Link>
-)}
+    <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
+      <div key={node.fields.slug}>
+        <h3>{node.frontmatter.title || node.fields.slug}</h3>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: node.frontmatter.description || node.excerpt,
+          }}
+        />
+      </div>
+    </Link>
+  );
+};
