63ea8b787b236fbf3f459940453ffc16d2598c83
youthfulhps <ybh942002@gmail.com>
2024-06-21 13:56:39 +0900

    Update: meta-config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -4,7 +4,7 @@ module.exports = {
   author: `youthfulhps`,
   introduction: `프론트엔드 주니어 개발자, 트러블슈팅에 대한 고민의 흔적을 기록으로 남기는 것을 좋아합니다.`,
   siteUrl: `https://youthfulhps.dev`,
-  thumbnail: 'static/banner.png',
+  thumbnail: '/banner.png',
   social: {
     twitter: ``,
     github: `youthfulhps`,
